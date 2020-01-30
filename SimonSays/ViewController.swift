5856a61bbb5258c8fa46f7e0a39f9bdce11baef9
AliWindsor <aliwindsor@gmail.com>
2020-01-30 17:15:07 +0000

    Initial Commit

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Alicia Windsor on 30/01/2020.
+//  Copyright © 2020 Alicia Windsor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
